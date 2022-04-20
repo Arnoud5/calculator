0467e376bb6108fc64c9d48694ee75245fffc5e3
Arnoud5 <rey.sarayar@gmail.com>
2022-04-20 17:51:06 +0700

    add delete and clear function

--- script.js
+++ script.js
@@ -1,68 +1,47 @@
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
-const btn4 = document.querySelector("#btn4");
-const btn5 = document.querySelector("#btn5");
-const btn6 = document.querySelector("#btn6");
-const btn7 = document.querySelector("#btn7");
-const btn8 = document.querySelector("#btn8");
-const btn9 = document.querySelector("#btn9");
-const btn0 = document.querySelector("#btn0");
-const btnTimes = document.querySelector("#btnTimes");
-const btnMin = document.querySelector("#btnMin");
-const btnEqual = document.querySelector("#btnEqual");
-const btnSum = document.querySelector("#btnSum");
-const btnDot = document.querySelector("#btnDot");
-const btnSub = document.querySelector("#btnSub");
-const btnClear = document.querySelector("#btnCls");
+const btn = document.querySelectorAll("#numBtn");
+const current = document.querySelector("#current-operand");
+const previus = document.querySelector("#previus-operand");
+const btnDelete = document.querySelector("#delete");
+const btnClear = document.querySelector("#clear");
+const opBtn = document.querySelectorAll("#operatorNum");
+const eq = document.querySelector("#eq");
+let currentNum = "";
+let previusNum = "";
+let operatorChosen = "";
+let operatorChosen2 = "";
+let result = "";
 
-btn1.onclick = () => alert("click 1");
-btn2.onclick = () => alert("click 1");
-btn3.onclick = () => alert("click 1");
-btn4.onclick = () => alert("click 1");
-btn5.onclick = () => alert("click 1");
-btn6.onclick = () => alert("click 1");
-btn7.onclick = () => alert("click 1");
-btn8.onclick = () => alert("click 1");
-btn9.onclick = () => alert("click 1");
-btn0.onclick = () => alert("click 1");
-btnMin.onclick = () => alert("click 1");
-btnEqual.onclick = () => alert("click 1");
-btnSum.onclick = () => alert("click 1");
-btnDot.onclick = () => alert("click");
-btnTimes.onclick = () => alert("click");
-btnSub.onclick = () => alert("click");
+eq.addEventListener("click", () => {
+  result = operate(operatorChosen, previusNum, currentNum);
+  previusNum = result;
+  previus.innerHTML = previusNum;
+  currentNum = "";
+  current.innerHTML = currentNum;
+});
 
-function add(a, b) {
-  return a + b;
-}
+opBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    operatorChosen = btn.value;
+    previusNum += currentNum;
+    if (currentNum == "") {
+      previus.innerHTML = `${previusNum} ${operatorChosen}`;
+    } 
+    // else if (currentNum && currentNum) {
 
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  if (a === 0 && b === 0) {
-    return "error";
-  }
-  if (b === 0) {
-    return "undefined";
-  }
-  if (a === 0) {
-    return "infinity";
-  }
-
-  return a / b;
-}
+    // } 
+    else {
+      previus.innerHTML = `${currentNum} ${operatorChosen}`;
+    }
+    currentNum = "";
+    current.innerHTML = currentNum;
+  });
+});
 
 function operate(operator, a, b) {
   a = Number(a);
   b = Number(b);
-    switch (operator) {
+
+  switch (operator) {
     case "+":
       return add(a, b);
     case "-":
@@ -73,3 +52,44 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+function add(a, b) {
+  return a + b;
+}
+function substract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (a === 0 && b === 0) {
+    return "error";
+  }
+  if (b === 0) {
+    return "undefined";
+  }
+  if (a === 0) {
+    return "infinity";
+  }
+  return a / b;
+}
+
+btn.forEach((numBtn) => {
+  numBtn.addEventListener("click", () => {
+    currentNum += numBtn.value;
+    current.innerHTML = currentNum;
+  });
+});
+
+btnDelete.addEventListener("click", () => {
+  currentNum = currentNum.slice(0, -1);
+  current.innerHTML = currentNum;
+});
+
+btnClear.addEventListener("click", () => {
+  currentNum = "";
+  previusNum = "";
+  current.innerHTML = currentNum;
+  previus.innerHTML = previusNum;
+});
